91b319c118f9a3017a9a8a5721f6498d940198d3
Soham Sadhukhan <sohamsadhukhan247@gmail.com>
2026-02-08 19:36:07 +0530

    feat: add Hero component and Landing page with Dashboard

---
+++ client/src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+import Hero from '../components/Hero';
+
+function Landing() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Hero />
+    </div>
+  )
+}
+
+export default Landing
